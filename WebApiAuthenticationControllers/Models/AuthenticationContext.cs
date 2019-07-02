0568bfe1c1ff3bdc5811b71011d1c5c328264b67
RushaliSoni <rushalilangaliya2@gmail.com>
2019-07-02 19:31:06 +0530

    registration entry using web api run in postman to check

---
+++ WebApiAuthenticationControllers/Models/AuthenticationContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApiAuthenticationControllers.Models
+{
+    public class AuthenticationContext : IdentityDbContext
+    {
+        public AuthenticationContext(DbContextOptions option) : base (option)
+        {
+
+        }
+
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+    }
+}
